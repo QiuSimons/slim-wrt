2cea8e17702eee3882d4bc7a19c14dc7128fac5d
JSZMonkey <22406031+JSZMonkey@users.noreply.github.com>
2021-09-04 21:36:54 +0800

    Update boostupnp.sh

--- slimapps/application/luci-app-boostupnp/root/usr/sbin/boostupnp.sh
+++ slimapps/application/luci-app-boostupnp/root/usr/sbin/boostupnp.sh
@@ -7,8 +7,8 @@ upnplog(){
 }
 
 # get public ip address
-ip=$(curl -sL --connect-timeout 3 whatismyip.akamai.com)
-#upnplog "My public IP address is: $ip"
+ip=$(curl -s https://api.ipify.org)
+upnplog "My public IP address is: $ip"
 
 # check public ip
 if [ -z "$ip" ];then
@@ -18,8 +18,7 @@ fi
 
 
 if expr "$ip" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*$' >/dev/null; then
-:
-#  upnplog "Validate public IP"
+  upnplog "Validate public IP"
 else
   upnplog "Wrong IP address"
   exit
@@ -27,11 +26,11 @@ fi
 
 # get upnp configed external ip
 upnp_ext_ip=$(uci get upnpd.config.external_ip)
-#upnplog "My upnp external IP address is: $upnp_ext_ip"
+upnplog "My upnp external IP address is: $upnp_ext_ip"
 
 
 if [ "$ip" = "$upnp_ext_ip" ];then
-#    upnplog "Upnp external IP up to date. Exit."
+    upnplog "Upnp external IP up to date. Exit."
     exit
 fi
 
@@ -41,6 +40,5 @@ uci commit upnpd
 
 # restart upnpd
 /etc/init.d/miniupnpd restart &
-upnplog "My public IP address is: $ip"
-upnplog "My upnp external IP address is: $upnp_ext_ip"
+
 upnplog "Configed new external for upnp: $ip"
